b5311db3a862308840c37f4dd1d49ad928774ee6
crisstinam <33080364+crisstinam@users.noreply.github.com>
2019-05-31 05:24:20 +0300

    update

--- Ora5-copy/Chat.java
+++ Ora5-copy/Chat.java
@@ -1,7 +1,14 @@
-public class Chat{
+import java.util.*;
 
-    public static void main(String []args){
-        String comanda;
+public class Chat{
+    
+    public static void main(String[]args){
+        Scanner s= new Scanner(System.in);
+        String mesaj;
+        System.out.println("Please insert message: ");
+        mesaj= s.nextLine();
+        System.out.println("Your message: "+mesaj);
+        
     }
 
 }
