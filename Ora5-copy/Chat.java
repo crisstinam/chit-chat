cecdcdec8e227cdc10f740affd148b24d2fff109
crisstinam <33080364+crisstinam@users.noreply.github.com>
2019-05-31 06:07:50 +0300

    update

--- Ora5-copy/Chat.java
+++ Ora5-copy/Chat.java
@@ -4,11 +4,17 @@ public class Chat{
     
     public static void main(String[]args){
         Scanner s= new Scanner(System.in);
-        String mesaj;
-        System.out.println("Please insert message: ");
-        mesaj= s.nextLine();
-        System.out.println("Your message: "+mesaj);
+        String message;
         
+        while(true){
+          System.out.println("Please insert message: (or \"QUIT\" if you want to exit");
+          message= s.nextLine();
+          if(message.equals("QUIT")){
+              return;
+            }
+          
+          System.out.println("Your message: "+message);
+        }
     }
 
 }
